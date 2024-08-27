bcbb333f228c267c016dcd88697795612e4fe58b
YaS <sery@ciklum.com>
2024-08-27 14:51:26 +0200

    add Twig, Maker bundles

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -1,26 +1,34 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private $message = [
         "hello", 'hi', 'bye!',
     ];
 
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
     {
-        return new Response(implode(',', $this->message));
+        return  $this->render('/hello/index.html.twig', [
+            'messages' =>  $this->message,
+            'limit' => $limit
+        ]);
+//            new Response(
+//            implode(',', array_slice($this->message, 0, $limit))
+//        );
     }
 
-    #[Route('/message/{id}', name:'app_message')]
-    public function showMessage($id)
+    #[Route('/message/{id<\d+>}', name:'app_message', methods: ['GET'])]
+    public function showMessage($id): Response
     {
         try {
-            return new Response($this->message[$id]);
+            return $this->render('/hello/show_message.html.twig', ['message' => $this->message[$id]]);
+//            return new Response($this->message[$id]);
         }
         /*catch (ExceptionInterface $exception) {
             return new Response($exception->getMessage());
